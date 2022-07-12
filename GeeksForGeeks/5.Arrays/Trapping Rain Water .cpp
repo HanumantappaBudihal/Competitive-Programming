da14eaa61216450809e132fe83e416c5e8274f13
Hanumantappa Budihal <33819543+HanumantappaBudihal@users.noreply.github.com>
2022-07-13 04:06:36 +0530

    Create Trapping Rain Water .cpp

---
+++ GeeksForGeeks/5.Arrays/Trapping Rain Water .cpp
@@ -0,0 +1,142 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+        int res=0;
+        
+        for(int i=1;i<n-1;i++)
+        {
+            //Find the maximum element on it's left
+            
+            int left=arr[i];
+            
+            for(int j=0;j<i;j++)
+            {
+                left=max(left,arr[j]);
+            }
+            
+            //Find the maximum element on it's right
+            int right=arr[i];
+            
+            for(int j=i+1;j<n;j++)
+                right=max(right,arr[j]);
+                
+                res=res+(min(left,right)-arr[i]);
+            
+        }
+        
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
+
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+
+    // Function to find the trapped water between the blocks.
+    public:
+    long long trappingWater(int arr[], int n){
+ // initialize output
+    long long int result = 0;
+
+    // maximum element on left and right
+    long long int left_max = 0, right_max = 0;
+
+    // indices to traverse the array
+    long long int lo = 0, hi = n - 1;
+
+    while (lo <= hi) {
+        if (arr[lo] < arr[hi]) {
+            if (arr[lo] > left_max)
+                // update max in left
+                left_max = arr[lo];
+            else
+                // water on curr element = max - curr
+                result += left_max - arr[lo];
+            lo++;
+        }
+        else {
+            if (arr[hi] > right_max)
+                // update right maximum
+                right_max = arr[hi];
+            else
+                result += right_max - arr[hi];
+            hi--;
+        }
+    }
+
+    return result;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin >> t;
+    
+    while(t--){
+        int n;
+        
+        //size of array
+        cin >> n;
+        
+        int a[n];
+        
+        //adding elements to the array
+        for(int i =0;i<n;i++){
+            cin >> a[i];            
+        }
+        Solution obj;
+        //calling trappingWater() function
+        cout << obj.trappingWater(a, n) << endl;
+        
+    }
+    
+    return 0;
+}  // } Driver Code Ends
